265dbac95476793ef18f85ec84213f0b66bd1de6
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-05-11 09:39:39 -0700

    add bearer:

--- src/AspNetAuthorizationSample.Web/Program.cs
+++ src/AspNetAuthorizationSample.Web/Program.cs
@@ -1,4 +1,20 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                  options.Audience = "http://localhost:5001";
+                  options.Authority = "http://localhost:5002";
+                  options.RequireHttpsMetadata = false;
+                });
+builder.Services.AddAuthorization();
+
 WebApplication app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => "Hello World!")
+   .RequireAuthorization();
+
 app.Run();
